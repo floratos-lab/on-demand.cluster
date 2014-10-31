1ca5c908e071039445eced2dc3705f2a2d195db6
Zhou Ji <zhou.ji@yahoo.com>
2014-10-31 17:37:20 -0400

    improve ARACNE script: replace hard-coded names,etc

--- cloud.aracne.sh
+++ cloud.aracne.sh
@@ -1,36 +1,37 @@
 # this script starts the task from the client side, i.e. cygwin terminal
-# this script automates the process of running an aracne job on the dynamically created standard instance of google cloud
+# it automates the process of running an aracne job on the dynamically created standard instance of google cloud compute engine
 # in the other words, no persistence disk is created or maintained for this task
 
-gcloud config set project wise-mantra-567 # project name: geworkbench-Web
-gcloud compute instances create zj-test-instance --zone us-central1-a
-echo "created instance zj-test-instance ... `date`"
+readonly PROJECT_ID=wise-mantra-567 # project name: geworkbench-Web
+readonly INSTANCE_ZONE=us-central1-a
+readonly INSTANCE_NAME=zj-test-instance
+
+readonly ARACNE_SCRIPT=./run.aracne.sh
+readonly ARACNE_SOFTWARE_PACKAGE="./aracne2 ./usage.txt"
+readonly EXPRESSION_DATA_FILE=./Bcell-100.exp
+readonly HUBFILE=./hub.txt
+readonly ARACNE_CONFIG_FILE=./config_threshold.txt
+
+gcloud config set project $PROJECT_ID 
+gcloud compute instances create $INSTANCE_NAME --zone $INSTANCE_ZONE
+echo "instance $INSTANCE_NAME created at `date`"
 sleep 10 # if I start trying to copy files too soon, the connection will time out. But why? 
 
-echo start copying `date`
-# copy program files
-gcloud compute copy-files ./aracne2 zj-test-instance:~/. --zone us-central1-a
-gcloud compute copy-files ./usage.txt zj-test-instance:~/. --zone us-central1-a
-gcloud compute copy-files ./run.aracne.sh zj-test-instance:~/. --zone us-central1-a
-
-# copy data files
-gcloud compute copy-files ./Bcell-100.exp zj-test-instance:~/. --zone us-central1-a
-gcloud compute copy-files ./config_threshold.txt zj-test-instance:~/. --zone us-central1-a
-gcloud compute copy-files ./hub.txt zj-test-instance:~/. --zone us-central1-a
-
-echo done with copying `date`
+echo start copying files`date`
+# copy program files and data files
+gcloud compute copy-files $ARACNE_SCRIPT $ARACNE_SOFTWARE_PACKAGE $EXPRESSION_DATA_FILE $HUBFILE $ARACNE_CONFIG_FILE $INSTANCE_NAME:~/. --zone $INSTANCE_ZONE
+echo finished copying `date`
 
 # run aracne
-gcloud compute ssh zj-test-instance --zone us-central1-a --command ./run.aracne.sh
+gcloud compute ssh $INSTANCE_NAME --zone $INSTANCE_ZONE --command $ARACNE_SCRIPT
 
 # copy the result back
 rm -r -f results
 mkdir results
-gcloud compute copy-files zj-test-instance:~/output.* ./results --zone us-central1-a
-gcloud compute copy-files zj-test-instance:~/*.adj ./results --zone us-central1-a
+gcloud compute copy-files $INSTANCE_NAME:~/output.* $INSTANCE_NAME:~/*.adj ./results --zone $INSTANCE_ZONE
 
 # delete the VM instance
-gcloud compute instances delete zj-test-instance --zone us-central1-a -q
+gcloud compute instances delete $INSTANCE_NAME --zone $INSTANCE_ZONE -q
 
 # check the results
 ls -lt results/
